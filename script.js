22e6b1bfc3d6ba2bef6f72d776ffb9bffe554dc5
v-loic44 <vandoorenloic2@hotmail.fr>
2022-03-23 00:05:20 +0000

    Projet final

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML= "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+
+
+    if(principal.value <= 0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
 }
         
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
